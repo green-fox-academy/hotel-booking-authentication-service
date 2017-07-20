e832942bbfaf37b1c0cf3f2ad4db8f6b56cb8504
Dombovari Peter <dombo.peter@gmail.com>
2017-07-20 09:24:42 +0200

    refact

--- src/main/java/com/greenfox/controller/AuthController.java
+++ src/main/java/com/greenfox/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.greenfox.exception.InvalidPasswordException;
 import com.greenfox.exception.NoSuchAccountException;
 import com.greenfox.model.Account;
 import com.greenfox.model.Heartbeat;
+import com.greenfox.model.RequestData;
 import com.greenfox.repository.AccountRepository;
 import com.greenfox.service.AuthService;
 import com.greenfox.service.GsonService;
@@ -13,8 +14,10 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -51,29 +54,59 @@ public class AuthController {
   public ResponseEntity heartbeat(HttpServletRequest request) {
     try {
       jwtAuthentication.attemptAuthentication(request);
-      System.out.println("accepted");
-      System.out.println("URI: " + request.getRequestURI());
       RestTemplate restTemplate = new RestTemplate();
       ResponseEntity response = restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Heartbeat.class);
-      System.out.println(response);
       return response;
     } catch (Exception e) {
-      System.out.println(e.getMessage());
       return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
   }
 
 
   @GetMapping("/api/users")
-  public ResponseEntity validate(HttpServletRequest request) {
+  public ResponseEntity getUsers(HttpServletRequest request) {
     try {
       jwtAuthentication.attemptAuthentication(request);
-      System.out.println("accepted");
-      System.out.println("URI: " + request.getRequestURI());
       RestTemplate restTemplate = new RestTemplate();
-      ResponseEntity response = restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Object.class);
-      System.out.println(response);
-      return response;
+      return restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Object.class);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  @GetMapping("/api/users/{userId}")
+  public ResponseEntity getUser(HttpServletRequest request) {
+    try {
+      jwtAuthentication.attemptAuthentication(request);
+      RestTemplate restTemplate = new RestTemplate();
+      return restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Object.class);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  @DeleteMapping("/api/users/{userId}")
+  public ResponseEntity deleteUser(HttpServletRequest request) {
+    try {
+      jwtAuthentication.attemptAuthentication(request);
+      RestTemplate restTemplate = new RestTemplate();
+      restTemplate.delete("http://" + userRepoServiceDomain + request.getRequestURI());
+      return new ResponseEntity<>("{}",HttpStatus.OK);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  @PutMapping("/api/users/{userId}")
+  public ResponseEntity updateUser(HttpServletRequest request, @RequestBody RequestData requestData) {
+    try {
+      jwtAuthentication.attemptAuthentication(request);
+      RestTemplate restTemplate = new RestTemplate();
+      restTemplate.put(
+          "http://" + userRepoServiceDomain + request.getRequestURI(), requestData);
+      return new ResponseEntity<>("{}",HttpStatus.OK);
     } catch (Exception e) {
       System.out.println(e.getMessage());
       return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
