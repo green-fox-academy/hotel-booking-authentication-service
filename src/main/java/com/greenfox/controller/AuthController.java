31ac8f2c3d2b57561d1fe30086b25bd01ea00b35
Dombovari Peter <dombo.peter@gmail.com>
2017-07-20 15:19:05 +0200

    refact: remove unecessary code, handle client CRUD requests in one endpoint

--- src/main/java/com/greenfox/controller/AuthController.java
+++ src/main/java/com/greenfox/controller/AuthController.java
@@ -1,97 +1,38 @@
 package com.greenfox.controller;
 
 import com.greenfox.model.Account;
-import com.greenfox.model.Heartbeat;
-import com.greenfox.model.RequestData;
-import com.greenfox.repository.AccountRepository;
-import com.greenfox.service.AuthService;
-import com.greenfox.service.GsonService;
 import com.greenfox.service.JwtAuthentication;
-import com.greenfox.service.JwtUnit;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 import javax.servlet.http.HttpServletRequest;
-
 
 @RestController
 public class AuthController {
 
   private String userRepoServiceDomain = System.getenv("userRepoServiceDomain");
-
-  private AccountRepository accountRepository;
-  private JwtUnit jwtUnit;
-  private GsonService gsonService;
-  private AuthService authService;
   private JwtAuthentication jwtAuthentication;
 
-  private Account credentials;
-  private Account responseAccount;
-  private String response;
-
   @Autowired
-  public AuthController(AccountRepository accountRepository, JwtUnit jwtUnit,
-                        GsonService gsonService, AuthService authService, JwtAuthentication jwtAuthentication) {
-    this.jwtUnit = jwtUnit;
-    this.accountRepository = accountRepository;
-    this.gsonService = gsonService;
-    this.authService = authService;
+  public AuthController(JwtAuthentication jwtAuthentication) {
     this.jwtAuthentication = jwtAuthentication;
   }
 
-  @GetMapping("/heartbeat")
-  public ResponseEntity heartbeat(HttpServletRequest request) {
+  @RequestMapping({"/api/users","/api/users/{userId}"})
+  public ResponseEntity crud(HttpServletRequest servletRequest, HttpEntity entityRequest, @PathVariable(required = false) Long userId) {
     try {
-      jwtAuthentication.attemptAuthentication(request);
-      RestTemplate restTemplate = new RestTemplate();
-      ResponseEntity response = restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Heartbeat.class);
-      return response;
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-  }
-
-
-  @GetMapping("/api/users")
-  public ResponseEntity getUsers(HttpServletRequest request) {
-    try {
-      jwtAuthentication.attemptAuthentication(request);
-      RestTemplate restTemplate = new RestTemplate();
-      return restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Object.class);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-  }
-
-  @GetMapping("/api/users/{userId}")
-  public ResponseEntity getUser(HttpServletRequest request) {
-    try {
-      jwtAuthentication.attemptAuthentication(request);
-      RestTemplate restTemplate = new RestTemplate();
-      return restTemplate.getForEntity("http://" + userRepoServiceDomain + request.getRequestURI(), Object.class);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-  }
-
-  @DeleteMapping("/api/users/{userId}")
-  public ResponseEntity deleteUser(@PathVariable int userId, HttpServletRequest request) {
-    try {
-      jwtAuthentication.attemptAuthentication(request);
-      String authenticationHeaderContent = request.getHeader("Authorization").substring(7);
-      System.out.println("Auth header: " + authenticationHeaderContent);
-      Account currentAccount = jwtUnit.parseToken(authenticationHeaderContent);
-      System.out.println("Account done");
-      if (!currentAccount.isAdmin() && currentAccount.getId() != userId) {
+      Account account = jwtAuthentication.attemptAuthentication(servletRequest);
+      if (!account.isAdmin() && account.getId() != userId) {
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
       } else {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.delete("http://" + userRepoServiceDomain + request.getRequestURI());
-        return new ResponseEntity<>("{}", HttpStatus.OK);
+        return restTemplate.exchange(URI.create("http://" + userRepoServiceDomain + servletRequest.getRequestURI()), HttpMethod.valueOf(servletRequest.getMethod()),
+            entityRequest, Object.class);
       }
     } catch (Exception e) {
       System.out.println(e.getMessage());
@@ -99,45 +40,9 @@ public class AuthController {
     }
   }
 
-  @PutMapping("/api/users/{userId}")
-  public ResponseEntity updateUser(HttpServletRequest request, @RequestBody RequestData requestData) {
-    try {
-      jwtAuthentication.attemptAuthentication(request);
-      RestTemplate restTemplate = new RestTemplate();
-      restTemplate.put(
-              "http://" + userRepoServiceDomain + request.getRequestURI(), requestData);
-      return new ResponseEntity<>("{}", HttpStatus.OK);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-  }
-
-  @PostMapping(value = "/register", produces = "application/json")
-  public ResponseEntity saveAccount(HttpServletRequest request, @RequestBody RequestData requestData) throws Exception {
-    try {
-      jwtAuthentication.attemptAuthentication(request);
-      RestTemplate restTemplate = new RestTemplate();
-      restTemplate.put(
-          "http://" + userRepoServiceDomain + request.getRequestURI(), requestData);
-      return new ResponseEntity<>("{}",HttpStatus.OK);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-  }
-
-  @PostMapping(value = "/login", produces = "application/json")
-  public ResponseEntity authenticateAccount(HttpServletRequest request, @RequestBody RequestData requestData) throws Exception {
-    try {
-      jwtAuthentication.attemptAuthentication(request);
-      RestTemplate restTemplate = new RestTemplate();
-      restTemplate.put(
-          "http://" + userRepoServiceDomain + request.getRequestURI(), requestData);
-      return new ResponseEntity<>("{}",HttpStatus.OK);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
+  @PostMapping(value = {"/register", "/login"}, produces = "application/json")
+  public ResponseEntity saveAccount(HttpServletRequest request, @RequestBody String json) throws Exception {
+      return new RestTemplate().postForEntity(
+          "http://" + userRepoServiceDomain + request.getRequestURI(), json, Object.class);
   }
 }
--- src/test/java/com/greenfox/service/JwtUnitTest.java
+++ src/test/java/com/greenfox/service/JwtUnitTest.java
@@ -15,6 +15,7 @@ public class JwtUnitTest {
   public void setup() {
 
     a.setAdmin(true);
+    a.setId(2L);
     a.setEmail("dombo.peter@gmail.com");
   }
 
@@ -23,13 +24,17 @@ public class JwtUnitTest {
   String token = jwtUnit.generateToken(a);
   System.out.println(token);
   Account a = jwtUnit.parseToken(token);
+    System.out.println(a.getId());
+    System.out.println(a.getEmail());
+    System.out.println(a.getToken());
+    System.out.println(a.getPassword());
   assertTrue(a.isAdmin());
   assertEquals("dombo.peter@gmail.com", a.getEmail());
   }
 
   @Test
   public void parseToken_Verified() throws Exception {
-    String validToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkb21iby5wZXRlckBnbWFpbC5jb20iLCJhZG1pblJvbGUiOnRydWV9.LuQX23fxwU0nEj4viCFPsuBr9KecC4xqlYY8eiRZRJl6-ac5-F22ihqyVrcVd2_g82LjxWxDJ1N-JrVvfLB2EQ";
+    String validToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkb21iby5wZXRlckBnbWFpbC5jb20iLCJ1c2VySWQiOiIyIiwiYWRtaW5Sb2xlIjpmYWxzZX0.mYdVE_FMflTOzR8jA3zMP0E8SfM_LVoeBDvq6sV4C0uNCT7B3IVxK2bUbXr2szFF2uO6wEV_cuFqikb2fxwO2Q\n";
     Account a = jwtUnit.parseToken(validToken);
     assertTrue(a.isAdmin());
     assertEquals("dombo.peter@gmail.com", a.getEmail());
